92ca63b09a2517476beeef1eacf0e68bd02f74cf
Benoit Delaunay <delaunay.ben@gmail.com>
2023-11-16 22:10:30 +0100

    fix(setup): long_description open encoding needed to be a named arg

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ import io
 import setuptools
 from setuptools import setup
 
+
 setup(
     name="aep_parser",
     version="0.0.1",
     author="Benoit Delaunay",
     author_email="delaunay.ben@gmail.com",
     description="A .aep (After Effects Project) parser",
-    long_description=io.open("README.md", "r", "utf-8").read(),
+    long_description=io.open("readme.md", mode="r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
