6d52a61404572cc65d0385bfa50fb2431d9e5329
Benoit Delaunay <delaunay.ben@gmail.com>
2023-11-17 17:46:47 +0100

    fix setup.py install_requires, add some classifiers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aep_parser",
-    version="0.0.5",
+    version="0.0.1",
     author="Benoit Delaunay",
     author_email="delaunay.ben@gmail.com",
     description="A .aep (After Effects Project) parser",
@@ -18,19 +18,33 @@ setup(
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: File Formats",
         "Topic :: Multimedia :: Graphics",
     ],
     install_requires=[
         "kaitaistruct>=0.9",
+        "enum34 ; python_version<'3.4'",
+        "future ; python_version<'3.0'",
     ],
-    extras_require={
-        ":python_version == '2.7'": [
-            "enum34",
-            "future",
-        ],
-    },
     python_requires=">=2.7",
+    options={
+        "bdist_wheel": {
+            "universal": True
+        }
+    }
 )
